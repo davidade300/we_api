d5c16d3ec592d7341cd2807807ca7f0124ac2b9b
davidade300 <iaderinho@icloud.com>
2025-03-10 23:04:57 -0300

    main.py file and notes about path params added

--- django_ninja/main.py
+++ django_ninja/main.py
@@ -1,4 +1,13 @@
+from enum import Enum
+
 from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
@@ -14,5 +23,25 @@ async def read_item(item_id: int):
     return {"item_id": item_id}
 
 
-# Query parameter operation
-@app.get("/items/")
+# Path parameter operation
+# those are evaluated in order, so if you have a fixed path that returns information
+# about the current user or something like that, you need to make sure that the path
+# for the fixed operation is declared before the dynamic path operation otherwise, the
+# dynamic path would also match the fixed path "thinking" that it's receiving a parameter
+# with the fixed value as its value.
+@app.get("/users")
+async def read_users():
+    return ["Bean", "Elfo"]
+
+
+# If you have a path operation that receives a path parameter,
+# but you want the possible valid path parameter values to be predefined,
+# you can use a standard Python Enum
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "DeepLearning for the win"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
